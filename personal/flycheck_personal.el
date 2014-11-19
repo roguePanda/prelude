26068ce8dc69d84d697c3206182ed1c5ccdc122e
Ben Navetta <ben.navetta@gmail.com>
2014-11-19 10:10:42 -0500

    set up osx keybindings

---
+++ personal/flycheck_personal.el
@@ -0,0 +1,11 @@
+;;; Code
+
+;;; Use arrow keys. We have them for a reason.
+(setq prelude-guru nil)
+
+;;; Command is easier to reach than Control on a Mac keyboard
+(setq mac-command-modifier 'control)
+(setq mac-control-modifier 'super)
+
+(provide 'personal)
+;;; personal.el ends here
---
+++ personal/preload/osx-keys.el
@@ -0,0 +1,8 @@
+;;; Code
+
+;;; Command is easier to reach than Control on a Mac keyboard
+(setq mac-command-modifier 'control)
+(setq mac-control-modifier 'super)
+
+(provide 'osx-keys)
+;;; osx-keys.el ends here
