b36b32d0f5a95686563bffc1878ca213c96dcfaa
Ben Navetta <ben.navetta@gmail.com>
2014-12-05 23:21:44 -0500

    fix comment error in personal.el

--- personal/personal.el
+++ personal/personal.el
@@ -1,6 +1,12 @@
-;;; Code
+;;; personal.el --- My personal tweaks
 
-;;; Use arrow keys. We have them for a reason.
+;;; Commentary:
+
+; I prefer 4-space tabs for indentation
+
+;;; Code:
+
+; Use arrow keys. We have them for a reason.
 (setq prelude-guru nil)
 
 ;;; Always show line numbers
@@ -10,10 +16,14 @@
 ; (setq mac-command-modifier 'control)
 ; (setq mac-control-modifier 'super)
 
+;;; Tab Settings
+(setq tab-width 4)
+(setq indent-tabs-mode t)
+
 ;;; Use redcarpet for Markdown
 (defun markdown-custom ()
-  "markdown-mode-hook"
-  (setq markdown-command "redcarpet --smarty") # TODO: pandoc
+  "Hook into markdown-mode to configure the Markdown implementation."
+  (setq markdown-command "redcarpet --smarty") ;  TODO: pandoc
   (setq markdown-enable-math t))
 (add-hook 'markdown-mode-hook '(lambda () (markdown-custom)))
 
